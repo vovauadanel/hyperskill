ea2d37a803c0f529ae63847211ca838ac568c44f
volodymyr <vovauadanel@gmail.com>
2022-02-15 08:03:28 +0200

    complete stage 2 hangman

--- hangman.py
+++ hangman.py
@@ -1,3 +1,7 @@
 # Write your code here
 print("H A N G M A N")
-print("The game will be available soon.")
+word = input("Guess the word: ")
+if word == "python":
+    print("You survived!")
+else:
+    print("You lost!")
