1b7826978bcd9cdadb6fe885cb609240044c8ce0
volodymyr <vovauadanel@gmail.com>
2022-02-17 19:01:24 +0200

    complete stage 3 hangman

--- hangman.py
+++ hangman.py
@@ -1,7 +1,10 @@
 # Write your code here
+import random
+
 print("H A N G M A N")
 word = input("Guess the word: ")
-if word == "python":
+right_words = ["python", "java", "kotlin", "javascript"]
+if word == random.choice(right_words):
     print("You survived!")
 else:
     print("You lost!")
